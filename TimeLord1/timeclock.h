5b0eda169fb06dccd5dc1f4f6ebb69a154306cb3
Louis Meadows <louis.meadows@mobifoundry.com>
2015-05-26 13:54:15 -0700

    Fat Tuesdays Code

--- TimeLord1/timeclock.h
+++ TimeLord1/timeclock.h
@@ -2,12 +2,34 @@
 #define TIMECLOCK_H
 
 #include <QObject>
-
+#include <QDateTime>
 class TimeClock
 {
 
 public:
-    TimeClock();
+    Q_OBJECT
+    Q_PROPERTY( QString name READ getName WRITE setName NOTIFY nameChanged )
+    QString getName() const
+    {
+        return m_name;
+    }
+
+public slots:
+    void setName(QString name)
+    {
+        if (m_name == name)
+            return;
+
+        m_name = name;
+        emit nameChanged(name);
+    }
+
+signals:
+    QString m_name;
+    void nameChanged(QString name);
+
+
+    explicit TimeClock();
     void clockIn(QString empName);
     void clockOut(QString empName);
 };
